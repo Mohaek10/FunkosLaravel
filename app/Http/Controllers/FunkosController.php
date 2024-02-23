3cfe6c1d66169bac5d02533d32cb1d94ac4110d1
Mohaek10 <elkasmimoha@gmail.com>
2024-02-23 09:21:02 +0100

    Added controller

--- app/Http/Controllers/FunkosController.php
+++ app/Http/Controllers/FunkosController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Funko;
+use Exception;
 
 class FunkosController extends Controller
 {
-    //
+    public function index(Request $request){
+        $funkos = Funko::search($request->search)->orderBy('id', 'desc')->isDeleted()->paginate(10);
+        return response()->json($funkos);
+    }
 }
