f3c43ccb4e9e8d62c02494f096a586329a348d5c
Mohaek10 <elkasmimoha@gmail.com>
2024-02-26 11:36:45 +0100

    Added middleware

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ Route::get('/', function () {
 
 
 Route::resource('funkos', FunkosController::class);
-
+/*
 Route::get('funkos/{id}/image', [FunkosController::class, 'editImagen'])->name('funkos.editImagen');
 Route::patch('funkos/{id}/image', [FunkosController::class,'actualizarImagen'])->name('funkos.actualizarImagen');
-
+*/
 // Route::get('/funkos', [FunkosController::class, 'index'])->name('funkos.index');
 // Route::get('/funkos/create', [FunkosController::class, 'create'])->name('funkos.create');
 // Route::post('/funkos', [FunkosController::class, 'store'])->name('funkos.store');
@@ -33,3 +33,21 @@ Route::patch('funkos/{id}/image', [FunkosController::class,'actualizarImagen'])-
 // Route::delete('/funkos/{id}', [FunkosController::class, 'destroy'])->name('funkos.destroy');
 
 //Detalle de un funko
+
+Route::group(['prefix' => 'funkos'], function () {
+    Route::get('/', [FunkosController::class, 'index'])->name('funkos.index');
+    Route::get('/create', [FunkosController::class, 'create'])->name('funkos.create')->middleware('auth', 'admin');
+    Route::post('/', [FunkosController::class, 'store'])->name('funkos.store')->middleware('auth', 'admin');
+    Route::get('/{id}', [FunkosController::class, 'show'])->name('funkos.show');
+    Route::get('/{id}/edit', [FunkosController::class, 'edit'])->name('funkos.edit')->middleware('auth', 'admin');
+    Route::put('/{id}', [FunkosController::class, 'update'])->name('funkos.update')->middleware('auth', 'admin');
+    Route::delete('/{id}', [FunkosController::class, 'destroy'])->name('funkos.destroy')->middleware('auth', 'admin');
+    Route::get('/{id}/image', [FunkosController::class, 'editImagen'])->name('funkos.editImagen')->middleware('auth', 'admin');
+    Route::patch('/{id}/image', [FunkosController::class,'actualizarImagen'])->name('funkos.actualizarImagen')->middleware('auth', 'admin');
+
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
